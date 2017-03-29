bce85e351d8027e0b1f99b76151c6fc3c981bf8c
Lawrence <lawrence.vanderpool@gmail.com>
2017-03-28 23:29:22 -0500

    wire in detail and create form

--- catchem/core/urls.py
+++ catchem/core/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.InventoryItemList.as_view(), name='index'),
+    url(r'^create/$', views.InventoryItemCreate.as_view(), name='create'),
+    url(r'^detail/(?P<pk>[0-9]+)/$', views.InventoryItemDetail.as_view(), name='detail'),
 ]
